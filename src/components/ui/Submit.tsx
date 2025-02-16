d05bf94b7216803c172d5d2fc13c9128f7b40d4d
Jakab Perczel <92743956+japerc@users.noreply.github.com>
2025-02-16 02:56:42 +0100

    Added cards

--- src/components/ui/Submit.tsx
+++ src/components/ui/Submit.tsx
@@ -1,42 +1,66 @@
 import React from 'react';
 import { Client, handle_file } from "@gradio/client";
 import { Button, ButtonProps } from "@/components/ui/button";
+import { Crosshair } from "lucide-react";
 
-interface SubmitImageProps {
+interface SubmitImageProps
+  extends ButtonProps {
   image: null | string;
-  buttonProps?: ButtonProps;
+  setLabel: React.Dispatch<React.SetStateAction<string | null>>;
+  setConf: React.Dispatch<React.SetStateAction<number | null>>;
 }
 
-
-const SubmitImage: React.FC<SubmitImageProps> = ({ image, buttonProps }) => {
+const SubmitImage: React.FC<SubmitImageProps> = (
+  { image, setLabel, setConf, ...props }) => {
+  const [waiting, setWaiting] = React.useState(false);
 
   const handleSubmit = async () => {
     try {
       const client = await Client.connect("japerc/kylix-fragment-classifier");
       if (!image) {
-        alert("Please upload an image first");
+        alert("Please upload an image first!");
       }
       else {
+        setWaiting(true);
         const response = await fetch(image);
         const blob = await response.blob();
         const file = new File([blob], "image.jpg", { type: blob.type });
         const result = await client.predict("/predict", { img: handle_file(file) });
+        setWaiting(false);
         if (result.type === "data") {
           const data = result.data as { confidences: { label: string, confidence: number }[] }[];
-          alert(data[0].confidences[0].label + ' ' + String(data[0].confidences[0].confidence));
+          setLabel(data[0].confidences[0].label)
+          setConf(Number(data[0].confidences[0].confidence.toFixed(2)))
         }
         else {
           console.log(result);
+          alert("Unknown response type: Please check the console for more information.");
         }
       }
     } catch (error) {
       console.log(error);
+      alert("An error occurred. Please check the console for more information.")
     }
-    return null;
   };
+
+  if (waiting) {
+    return (
+      <Button {...props} disabled>
+        <Crosshair className="animate-spin" />
+        Please wait
+      </Button>
+    )
+  }
+  if (!image) {
+    return (
+      <Button onClick={handleSubmit} {...props} disabled>
+        <Crosshair /> Predict
+      </Button>
+    )
+  }
   return (
-    <Button onClick={handleSubmit} {...buttonProps}>
-      Predict
+    <Button onClick={handleSubmit} {...props}>
+      <Crosshair /> Predict
     </Button>
   )
 }
